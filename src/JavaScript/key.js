95932f249102bbc116c8f135e83e5b86c1f40b7f
Fanttatraieste <118627923+Fanttatraieste@users.noreply.github.com>
2023-06-18 18:44:38 +0000

    Deploying to gh-pages from @ Fanttatraieste/JS-TeamProject-Team3@e36b37199123df3ce4d79bf92b19f4d6d92cb6f2 🚀

--- src/JavaScript/key.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  key: '8b218b85545392c9f8705c30fbfd1bce',
-};
--- src/index.js
+++
@@ -1,8 +0,0 @@
-const renderPages = require('./JavaScript/body');
-const blabla = require('./JavaScript/number'); //doar numele functiei
-const search = require('./JavaScript/header');
-
-search();
-blabla(); //se apeleaza functia
-
-//renderPages(5);
